ee3a03734c98865b5370fb497d7cd2761281b5fc
orsys <orsys@orsys.fr>
2023-03-17 16:50:33 +0100

    Component LifeCycle

--- projects/dashboard/src/app/app.component.ts
+++ projects/dashboard/src/app/app.component.ts
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor(){
-    console.log(this)
+    console.log(1,this)
+  }
+  ngOnInit(){
+    console.log(2,this)
+  }
+  ngDoCheck(){
+    console.log(3,this)
+  }
+  ngOnDestroy(){
+    console.log(4,this)
   }
 }
