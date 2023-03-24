f353ecc36a6c9c21a84704d37eb9a23b6f57c085
orsys <orsys@orsys.fr>
2023-03-24 15:23:48 +0100

    Misc

---
+++ projects/dashboard/src/theory/best-practices-es6.ts
@@ -0,0 +1,27 @@
+
+/* 
+
+API ES6/2015
+
+let, const, class
+
+Asynchronicité (en JavaScript) - Promise
+
+Pièges Syntaxiques :
+spread operator (...)
+object littéral
+
+modules 
+
+*/
+
+/* 
+
+Best Practices
+
+- Utiliser spread ou forOf ou le itération fonctionnelle
+- Refactoriser les callbacks
+- Isoler les try/catch dans des fonction dédiées !!!
+- Modulariser, Modulariser, Modulariser !!!
+
+*/
