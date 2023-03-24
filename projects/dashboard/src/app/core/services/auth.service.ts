05448876e21fa321903b262551dad636b8cbebbb
orsys <orsys@orsys.fr>
2023-03-24 16:54:25 +0100

    Store Connection

--- projects/dashboard/src/app/core/services/auth.service.ts
+++ projects/dashboard/src/app/core/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, of } from 'rxjs';
+import { EActionTypes } from '../enums/e-action-types';
 import { AppAuthCredentials } from '../interfaces/app-auth-credentials';
 import { AppAuthErrorResponse, AppAuthResponse, AppAuthValidResponse } from '../interfaces/app-auth-response';
+import { DataStoreService } from '../store/data-store.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,14 @@ export class AuthService {
   private AuthToken: string | null = null;
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private dataStore:DataStoreService
+  ) { 
+    this.dataStore.action$.subscribe( action => {
+      debugger;
+      if( action.type === EActionTypes.USER_LOGIN_REQUEST ) this.login(action.payload)
+    })
+  }
 
   public isAuth() {
     return this.AuthToken !== null;
@@ -40,8 +48,14 @@ export class AuthService {
 
   private processServerResponse = (response: AppAuthResponse) => {
 
-    if ((response as AppAuthValidResponse).access_token) this.AuthToken = (response as AppAuthValidResponse).access_token;
+    if ((response as AppAuthValidResponse).access_token !== undefined) {
 
+      this.AuthToken = (response as AppAuthValidResponse).access_token;
+      this.dataStore.dispatch({type:EActionTypes.USER_LOGIN_SUCCESS, payload:''})
+      return;
+    } 
+
+    this.dataStore.dispatch({type:EActionTypes.USER_LOGIN_FAILURE, payload:''})
   }
 
 }
